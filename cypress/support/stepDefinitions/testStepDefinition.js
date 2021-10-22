c8ea472f436cb846520013e7e63f5c47c048d227
alejandro9702 <ovejod12@hotmail.com>
2021-10-22 09:49:09 -0500

    set url in env variables and using in the test case

--- cypress/support/stepDefinitions/testStepDefinition.js
+++ cypress/support/stepDefinitions/testStepDefinition.js
@@ -2,6 +2,6 @@
 const {Given, When, Then} = require("cypress-cucumber-preprocessor/steps");
 
 Given('the user trying to login to quotes portal with the user {string} and password {string}', function(email,password) {
-    cy.visit("https://quote-qa.edgelogistics.com/main/quotes");
+    cy.visit(Cypress.env('url'));
     cy.loginAsAdmin(email,password);
 });
