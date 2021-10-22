f5881dccf96ef7f990e5bfbcdea1e14d4f5fea27
alejandro9702 <ovejod12@hotmail.com>
2021-10-21 23:33:53 -0500

    successful sigin step, just the given implementation

--- cypress/support/stepDefinitions/testStepDefinition.js
+++ cypress/support/stepDefinitions/testStepDefinition.js
@@ -1,9 +1,7 @@
 
 const {Given, When, Then} = require("cypress-cucumber-preprocessor/steps");
 
-Given('Start to type your Given step here', function(){
+Given('the user trying to login to quotes portal with the user {string} and password {string}', function(email,password) {
     cy.visit("https://quote-qa.edgelogistics.com/main/quotes");
-    cy.get("#dd").type("hola");
-    
-
+    cy.loginAsAdmin(email,password);
 });
